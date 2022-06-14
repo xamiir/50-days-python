ade3d498f9201c0a380915f2f054ca5c5327d656
cllaahi <cllaahicabdi04@gmail.com>
2022-06-14 20:51:56 +0300

    Up-to-date

---
+++ day-14/Main.py
@@ -0,0 +1,33 @@
+
+## practice if...else
+
+## statement one check = or !=
+a=int(input("Enter a number A"))
+b=int(input("Enter a number B "))
+if a==b:
+    print("a and are equals ")
+# elif a!=b:
+#        print("a and b are not equal ")
+elif a<b:
+    print("a is less then b")
+elif a<=b:
+        print("a is less then or equal to b")
+elif a>b:
+  print("a is greater then b")
+elif a>=b:
+        print("a is less then or equal to b")
+else:
+    print(" input user only a number that int try again enter a number ")
+
+# Assignment if student enter marks 
+# for example if less than 50 print out f 50 greter then or equal E
+# print f 60 greter then or equal D
+# print f 70 greter then or equal C
+# print f 80 greter then or equal B
+# print f 90 greter then or equal A
+
+    
+    
+    
+
+
