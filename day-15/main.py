a35c0743fcad998aa7f7187ad6e432e82218e66d
unknown <cllaahicabdi04@gmail.com>
2022-08-25 18:00:25 +0300

    new lesson

---
+++ day-15/main.py
@@ -0,0 +1,13 @@
+ marks=int(input(print("Enter a marks: ")))
+ if mark>=90:
+    print("yuor grade is :A")
+elif mark>=80:
+    print("your grade is :B")
+elif mark>=70:
+    print("your grade is :C")
+elif mark>=60:
+    print("your grade is :D")
+elif mark>=50:
+    print("your grade is :E")
+else:
+    print("your grade is :F")
